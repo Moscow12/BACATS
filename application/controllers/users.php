5b3c91daa65f83c377339ac2013f30eac15ba01d
Moscow12 <meshackstev@gmail.com>
2018-05-31 17:10:26 +0300

    modefing Code for register

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,14 +1,17 @@
 <?php 
 	 class Users extends CI_Controller {
 	 	public function register(){
-	 		$data['title'] = 'Sign Up';
+			 $data['title'] = 'Sign Up';
+			 
+			$data['roles'] = $this->user_model->register('role_id');
 	 		  $this->load->library('form_validation');
 
-	 		$this->form_validation->set_rules('name', 'Name', 'required');
-	 		$this->form_validation->set_rules('username', 'User Name', 'required|callback_check_username_exists');
-	 		$this->form_validation->set_rules('email', 'Email', 'required');
-	 		$this->form_validation->set_rules('password', 'Password', 'required');
-	 		$this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
+			  $this->form_validation->set_rules('firstname', 'First Name', 'required');
+			  $this->form_validation->set_rules('mname', 'Middle Name', 'required');
+			  $this->form_validation->set_rules('lastname', 'Last Name', 'required');
+	 		  $this->form_validation->set_rules('reg_no', 'User Name', 'required|callback_check_reg_no_exists');
+	 		//   $this->form_validation->set_rules('password', 'Password', 'required');
+	 		//   $this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
 			
 			if($this->form_validation->run() ===FALSE){
 				$this->load->view('templates/header');
@@ -17,19 +20,20 @@
 			} else{
 
 				#Encrept password
-				$enc_password = md5($this->input->post('password'));
+				$enc_password = md5($this->input->post('BACATS@2018'));
 				$data = array(
-				'name' => $this->input->post('name'),
-				'email' => $this->input->post('email'),
-				'username' => $this->input->post('username'),
-				'password' => $enc_password
-				
+				'firstname' => $this->input->post('firstname'),
+				'mname' => $this->input->post('mname'),
+				'lastname' => $this->input->post('lastname'),
+				'reg_no' => $this->input->post('reg_no'),
+				'password' => $enc_password,
+				'role_id' => 'role_id'			
 			);
 
 			//insert user
 
 				$this->user_model->register($data);
-
+				var_dump($data);
 				//set messege
 				$this->session->set_flashdata('user_register', 'You are now registered and can login');
 
@@ -43,7 +47,7 @@
 	 		  $this->load->library('form_validation');
 
 	 		
-	 		$this->form_validation->set_rules('username', 'User Name', 'required');
+	 		$this->form_validation->set_rules('reg_no', 'User Name', 'required');
 	 		$this->form_validation->set_rules('password', 'Password', 'required');
 	 		
 			
@@ -54,19 +58,19 @@
 			} else{
 
 
-					//Get username
-				$username = $this->input->post('username');
+					//Get reg_no
+				$reg_no = $this->input->post('reg_no');
 				//Get and encript the password
 				$password = md5($this->input->post('password'));
 
 				//login user
-				$user_id = $this->user_model->login($username, $password);
+				$user_id = $this->user_model->login($reg_no, $password);
 
 				if($user_id){
 					//create session
 					$user_data = array(
 						 'user_id' => $user_id,
-						 'username' => $username,
+						 'reg_no' => $reg_no,
 						 'logged_in' => true
 					);
 
@@ -98,16 +102,16 @@
 	 		//Unset user data
 	 		$this->session->unset_userdata('logged_in');
 	 		$this->session->unset_userdata('user_id');
-	 		$this->session->unset_userdata('username');
+	 		$this->session->unset_userdata('reg_no');
 
 	 		$this->session->set_flashdata('user_loggedout', 'You are now loged out login again');
 
 						redirect('index.php/users/login');
 	 	}
-	 	// check if username exists
-	 	public function check_username_exists($username){
-	 		$this->form_validation->set_message('check_username_exists', 'That username is taken. Choose another one');
-	 		if($this->user_model->check_username_exists($username)){
+	 	// check if reg_no exists
+	 	public function check_reg_no_exists($reg_no){
+	 		$this->form_validation->set_message('check_reg_no_exists', 'That reg_no is taken. Choose another one');
+	 		if($this->user_model->check_reg_no_exists($reg_no)){
 	 			return true;
 	 		} else{
 	 			return false;
