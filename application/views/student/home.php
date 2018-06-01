6d01e546a04620b16ff666eacc06094aeed16011
Moscow12 <meshackstev@gmail.com>
2018-06-01 21:13:58 +0300

    +st

---
+++ application/views/student/home.php
@@ -0,0 +1,16 @@
+<div class="card border-danger text-center">
+    <div class="card-header">
+        Read BACATS Terms
+    </div>
+  <div class="card-body">
+    <h5 class="card-title">Read terms and condition</h5>
+    <p class="card-text">This it the class system attendance tracking system.<br/> Students shall have the 
+    role to update/edit their profile and register course that undertake which are already registerd in the system.<br/> 
+    Also should register their finger prints
+    </p>
+    <a href="#" class="btn btn-primary">Register Course</a>
+  </div>
+  <div class="card-footer border-danger text-danger">
+   Bepunctual attend classes
+  </div>
+</div>
