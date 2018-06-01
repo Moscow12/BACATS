330a2b0392efc0811f767723f9b612cea73403a7
Moscow12 <meshackstev@gmail.com>
2018-06-01 21:11:22 +0300

    new model student

---
+++ application/models/Student_Model.php
@@ -0,0 +1,15 @@
+<?php 
+	class Student_model extends CI_Model{
+
+		public function __Construct(){
+			$this->load->database();
+        }
+        
+        public function get_content(){
+            
+        }
+
+        public function set_stprofile(){
+            
+        }
+    }
