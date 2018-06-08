a31601f13dd41b47742c0a9c6969a40b769d79f5
Moscow12 <meshackstev@gmail.com>
2018-06-08 03:44:45 +0300

    ++student model code

--- application/models/Student_Model.php
+++ application/models/Student_Model.php
@@ -5,7 +5,35 @@
 			$this->load->database();
         }
         
-        public function get_content(){
+        // public function get_content($id = null){
+        //     $this->db->select('*');
+        //     $this->db->from('users');
+        //     $this->db->join('role', "role.id = '2', ");
+        //     $this->db->join('users', "users.reg_no = '2015-04-08524', ");
+        //     $this->db->join('users', "users.password = 'BACATS@2018'");
+        //     $query = $this->db->get();
+        //     return $query->result_array();
+
+        //     // if (!is_null($id)) $this->db->where('id', $id);
+        //     // $this->db->order_by('id', 'desc');
+        //     // return $this->db->get()->result();
+       
+            
+        // }
+
+        public function get_content($id = null){
+            $this->db->select('*');
+            $this->db->from('users, role');
+            $this->db->where("'users.role_id' = role.id AND users.reg_no = '2015-04-00000' AND users.password = '00000000'");
+            
+            // return $this->db;
+            $query = $this->db->get();
+            return $query->result_array();
+
+            // if (!is_null($id)) $this->db->where('id', $id);
+            // $this->db->order_by('id', 'desc');
+            // return $this->db->get()->result();
+       
             
         }
 
