b7ac74a9c1c4d802c6e4a808a4369486e89b2743
Moscow12 <meshackstev@gmail.com>
2018-06-08 03:49:41 +0300

    /of file

--- application/views/admin/program.php
+++ application/views/admin/program.php
@@ -4,14 +4,10 @@
 
 <?php echo form_open('index.php/registered/program'); ?>
 <div class="mb-0 mt-4">
-         
-          <div class="card-columns">
-
-             
-            <div class="card mb-3">
+    <div class="card-columns">
+        <div class="card mb-3">
             <div class="card-header small text-muted"><h3><?php echo $title; ?></h3></div>
-           
-            <div class="form-group">
+                <div class="form-group">
             <label>Department name</label>
                 <select name="dept_id" class="form-control">
                     <?php foreach ($departments as $department){ ?>
@@ -28,21 +24,3 @@
           </div>
         </div>
 <?php echo form_close(); ?>
-
-<div class="mb-0 mt-4">    
-  <div class="card-columns">
-      <div class="card mb-3">
-          <div class="card-header small text-muted"> 
-              <h2>Program created</h2>
-          </div>
-           
-          <ul class="list-group">
-              <?php foreach ($programs as $program){ ?>
-              <li class="list-group-item"><?php echo $program['program_name']; ?>
-                 
-              </li>
-              <?php } ?>
-          </ul> 
-      </div>
-    </div>
-  </div>
