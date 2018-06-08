414e27db4413881a7a9a9fb9c6de80e7a3cad262
Moscow12 <meshackstev@gmail.com>
2018-06-08 03:39:46 +0300

    def routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,8 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-$route['default_controller'] = 'pages/view';
-$route['registered'] = 'registered/homep';
+$route['default_controller'] = 'users';
+
+//$route['registered'] = 'registered/homep';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
