588594d7bc459d6c2e4fd139765fca096079c08c
Moscow12 <meshackstev@gmail.com>
2018-06-10 14:57:14 +0300

    addition of link and user session data

--- application/views/student/home.php
+++ application/views/student/home.php
@@ -1,23 +1,19 @@
-<div class="card border-danger text-center">
-    <div class="card-header">
-        Read BACATS Terms
-    </div>
-  <div class="card-body">
-    <h5 class="card-title">Read terms and condition</h5>
-    <p class="card-text">This it the class system attendance tracking system.<br/> Students shall have the 
-    role to update/edit their profile and register course that undertake which are already registerd in the system.<br/> 
-    Also should register their finger prints
-    </p>
-    <a href="#" class="btn btn-primary">Register Course</a>
-  </div>
-  <div class="card-footer border-danger text-danger">
-   Bepunctual attend classes
-  </div>
+
+<?php if(!($this->session->userdata('user_id'))){
+	redirect('index.php/users/index');
+	}?>
+<div class="card">
+    <div class="card border-danger text-center">
+        <div class="card-header"><?php echo $title; ?></div>
+            <div class="card-body">
+                <h5 class="card-title">Read terms and condition</h5>
+                    <p class="card-text">This it the class system attendance tracking system.<br/> Students shall have the 
+                    role to update/edit their profile and register course that undertake which are already registerd in the system.<br/> 
+                    Also should register their finger prints
+                    </p>
+                <a href="enroll" class="btn btn-primary">Enroll Course</a>
+            </div>
+        <div class="card-footer border-danger text-danger">Bepunctual attend classes</div>
 </div>
 
- <!-- <?php foreach($query->result_array() as $row): ?>
-        <!-- <tr>
-            <td><#?php echo $row->role_name; ?></td>
-            <td><#?php echo $row->reg_no;?></td>
-        </tr> -->
-<?php endforeach; ?> -->
+ 
