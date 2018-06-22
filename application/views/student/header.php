e6d6bc0e962235e2f83112800f47ea739b97fa4b
Moscow12 <meshackstev@gmail.com>
2018-06-22 20:34:56 +0300

    bg-color

--- application/views/student/header.php
+++ application/views/student/header.php
@@ -18,9 +18,9 @@
   <link href="<?=base_url();?>assets/css/sb-admin.css" rel="stylesheet">
 </head>
 
-<body class="fixed-nav sticky-footer bg-warning" id="page-top">
+<body class="fixed-nav sticky-footer bg-light" id="page-top">
   <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-warning bg-warning fixed-top" id="mainNav">
+  <nav class="navbar navbar-expand-lg navbar-warning bg-light fixed-top" id="mainNav">
     <a class="navbar-brand" href="index.html">BACATS</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
