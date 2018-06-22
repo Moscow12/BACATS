54a96ce7d75618e80c0b71f0efc312796d3bc71b
Moscow12 <meshackstev@gmail.com>
2018-06-22 20:34:41 +0300

    teacher view

--- application/views/admin/teacher.php
+++ application/views/admin/teacher.php
@@ -7,7 +7,7 @@
 		<h3><?php echo $title; ?></h3>
 	</div>
 	<div class="card-body">
-		<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+		<table class="table table-bordered" id="mytable" width="100%" cellspacing="0">
 			<thead>
 				<tr>
 					<th rowspan="2">#</th>
@@ -17,20 +17,20 @@
 				
 				</tr>
 				<tr>
-					<th>View</th>
-					<th> Edit</th>
-					<th> Delete</th>
+					<th> <button class="btn btn-success"><i class = "fa fa-eye"></button></i></th>
+					<th> <button class="btn btn-primary"> <i class = "fa fa-edit"></button></i></th>
+					<th> <button class="btn btn-danger"> <i class = "fa fa-trash"></button></i></th>
 				</tr>
 			</thead>
 			
 			<?php foreach($teachers as $users){?>
 				<tr>
-					<th scope="row">1<?php #$count = $row; ?></th>
+					<td scope="row"><?php echo $users['id']; ?></td>
 					<td><?php echo $users['name']; ?> </td>
 					<td><?php echo $users['reg_no']; ?> </td>
-					<td ><button class="btn btn-success"><i class = "fa fa-eye"></button></i></td>
-					<td><button class="btn btn-primary"> <i class = "fa fa-edit"></button></i></td>
-					<td><button class="btn btn-danger"> <i class = "fa fa-trash"></button></i></td>
+					<td ><button class="btn btn-success">View</button></td>
+					<td><button class="btn btn-primary"> Edit</button></td>
+					<td><button class="btn btn-danger"> Delete</button></td>
 				
 			   	<tr><br/>
 			<?php }?>
@@ -38,5 +38,8 @@
 		<div class="card-footer small text-muted">Updated at <?php echo date('Y/m/d'); ?></div>
 	</div>
 </div>
+
 	
+	<script src="<?=base_url();?>assets/vendor/datatables/jquery.dataTables.js"></script>
+
 
