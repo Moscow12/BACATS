6c638b259a165ccaddf0379e571983046914538d
MOSCOW12 <meshackstev@gmail.com>
2018-06-27 18:11:17 -0700

    arrangement

--- application/views/student/regcourse.php
+++ application/views/student/regcourse.php
@@ -4,43 +4,45 @@
 	}?>
     <style>.table {
     border-radius: 5px;
-    width: 50%;
+    width: 99%;
     margin: 0px auto;
     float: none;
 }</style>
 	
 <?php echo form_open('index.php/Student/one'); ?>
-<div class="col-sm-10">
-    <div class="card">
-        <div class="card-header">
-            <h2><?php echo $title; ?></h2>
-        </div>
-        <div class="card-body">
-        <?php echo validation_errors(); ?>
-            <div class="justfie"></div>
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <tr>
-                    <th>Course Code</th>
-                    <th>Course name</th>
-                    <th>Instructed By</th>
-                    <th><button class="btn btn-primary"><i class="fa fa-check"></i></button ><span>Register</span></th>
-                </tr>
-                
-                    <?php foreach ($courses as $users){ ?>
-                        <tr>
-                            <td><?php echo $users['course_code']; ?></td>
-                            <td><?php echo $users['course_name']; ?></td>
-                            <td ><?php echo $users['Byname']; ?></td>
-                            <td align="center">
-                                <div class="form-check">
-                                    <input type="checkbox" name="course[]" value="<?php echo $course['id']; ?>" class="form-check-input" id="exampleCheck1" align="center">
-                                </div>
-                            </td>
+    <div class="row"><div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <div class="card">
+                <div class="card-header" align="center">
+                    <h3><?php echo $title; ?></h3>
+                </div>
+                <div class="card-body">
+                <?php echo validation_errors(); ?>
+                    <div class=""></div>
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <tr align="center">
+                            <th>Course Code</th>
+                            <th>Course name</th>
+                            <th>Instructed By</th>
+                            <th ><button class="btn btn-primary"><i class="fa fa-check"></i>Register</button ></th>
                         </tr>
-                    <?php } ?>
-                
-            </table>
-            <button type="submit" class="btn btn-primary">Confirm registration</button>
-        </div>
-    </div>
-</div>
+                        
+                            <?php foreach ($courses as $users){ ?>
+                                <tr>
+                                    <td><?php echo $users['course_code']; ?></td>
+                                    <td><?php echo $users['course_name']; ?></td>
+                                    <td ><?php echo $users['Byname']; ?></td>
+                                    <td align="center">
+                                        <div class="form-check">
+                                            <input type="checkbox" name="course[]" value="<?php #echo $course['id']; ?>" class="form-check-input" id="exampleCheck1" align="center">
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        
+                    </table>
+                    <button type="submit" class="btn btn-primary">Confirm registration</button>
+                </div>
+            </div>
+        </div><div class="col-sm-1"></div>
+    </div>    
